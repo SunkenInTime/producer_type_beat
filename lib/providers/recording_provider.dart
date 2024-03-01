e8c0beae82a8a22d4a82ad8bbd3a202011de8fe0
SunkenInTime <shawnadedeji@gmail.com>
2024-03-01 13:35:12 -0500

    Fixed file saving

--- lib/providers/recording_provider.dart
+++ lib/providers/recording_provider.dart
@@ -8,25 +8,33 @@ import 'package:record/record.dart';
 import 'package:path_provider/path_provider.dart';
 
 class RecordingProvider with ChangeNotifier {
-  final record = AudioRecorder();
   bool isRecording = false;
-
+  final record = AudioRecorder();
   void startRecording() async {
     try {
       final granted = await PermissionHelper.requestStoragePermissions();
       if (!granted) return;
+
       if (await record.hasPermission() && isRecording == false) {
         // Start recording to file
+
         String date = DateTime.now().toString();
         setRecordingStaus(true);
         String randomFileName = HelperFunctions().getRandomString(5);
-        log("/storage/emulated/0/Download/$date-$randomFileName.m4a");
+
         // Get the external storage directory
+
         Directory? directory = await getExternalStorageDirectory();
         String? storagePath = directory?.path;
 
         // Construct the file path
-        String filePath = '$storagePath/Download/$date-$randomFileName.m4a';
+        String filePath = '$storagePath/recordings/$date-$randomFileName.m4a';
+        Directory createDir = Directory('$storagePath/recordings/');
+        if (!createDir.existsSync()) {
+          createDir.createSync(recursive: true);
+        }
+
+        // await File(filePath).create();
         await record.start(const RecordConfig(), path: filePath);
         // ... or to stream
         // final stream = await record.startStream(const RecordConfig(AudioEncoder.pcm16bits));
@@ -48,7 +56,7 @@ class RecordingProvider with ChangeNotifier {
       final path = await record.stop();
       log(path!);
       setRecordingStaus(false);
-      record.dispose(); // As always, don't forget this one.
+      // record.dispose(); // As always, don't forget this one.
     } catch (e) {
       setRecordingStaus(false);
       log(e.toString());
